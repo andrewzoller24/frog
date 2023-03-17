0208283e7460900f81985e736df0b277e15e474f
Andrew Zoller <111621872+andrewzoller24@users.noreply.github.com>
2023-03-17 21:53:52 +0000

    Fri, Mar 17, 2023, 2:53 PM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -2,5 +2,12 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
-    btn.classList
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
 })
