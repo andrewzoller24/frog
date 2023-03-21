5741d1f8b9202ef1664bddc0ccee98262ff81e20
Andrew Zoller <111621872+andrewzoller24@users.noreply.github.com>
2023-03-21 21:46:30 +0000

    Tue, Mar 21, 2023, 2:46 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,11 @@
 const counters = document.querySelectorAll('counter')
+
+counters.ForEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+    }
+
+    updateCounter()
+})
