198a43ecfc4167d28ab89dbda68b8270618fc738
Andrew Zoller <111621872+andrewzoller24@users.noreply.github.com>
2023-03-21 21:53:25 +0000

    Tue, Mar 21, 2023, 2:53 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -4,8 +4,16 @@ counters.ForEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target')
+        const target = +counter.getAttribute('data-target')
+        const c = counter.innerText
+        const increment = target / 200
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        }
     }
 
     updateCounter()
 })
+
